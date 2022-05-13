1dd4a4ad257c00bfe1906c38ebf29969d638e5d4
ryrodrig112 <ryrodrig112@gmail.com>
2022-05-13 13:49:25 -0700

    Define stack, player, and dice classes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 print("Hello World")
 
+
 class Card:
     def __init__(self, color, number):
         self.color = color
@@ -7,6 +8,19 @@ class Card:
 
     def __str__(self):
         return "This card is a {color} {number}".format(color = self.color, number = self.number)
+
+
+class Stack:
+    pass
+
+
+class Player:
+    pass
+
+
+class Dice:
+    pass
+
 
 card = Card("Red", "6")
 print(card)
