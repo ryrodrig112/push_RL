dd93dd3eae4a613b68fc961f5a5824c3027085dd
ryrodrig112 <ryrodrig112@gmail.com>
2022-05-13 13:58:40 -0700

    Define numbered_cards

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ class Card:
         self.number = number
 
     def __str__(self):
-        return "This card is a {color} {number}".format(color = self.color, number = self.number)
+        return "This card is a {color} {number}".format(color=self.color, number=self.number)
 
 
 class Stack:
-    pass
+    def __init__(self, cards_in_stack):
+        self.cards_in_stack = cards_in_stack
 
 
 class Player:
@@ -22,5 +23,17 @@ class Dice:
     pass
 
 
-card = Card("Red", "6")
-print(card)
+colors = ['Red', 'Blue', 'Yellow','Green', 'Purple']
+numbers = [str(x) for x in range(1,7)]
+copies_of_number = 3
+
+all_cards = []
+for color in colors:
+    for number in numbers:
+        for i in range(copies_of_number):
+            card = Card(color, number)
+            all_cards.append(card)
+
+for card in all_cards:
+    print(card)
+assert len(all_cards) == 90
