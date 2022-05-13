c06db0f96ba90799539ecf786b99065b3871e633
ryrodrig112 <ryrodrig112@gmail.com>
2022-05-13 16:40:41 -0700

    Implement shuffle and pile availability

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-print("Hello World")
-import deck
+import push
 
+print("Hello World")
+print('')
 
 colors = ['Red', 'Blue', 'Yellow','Green', 'Purple']
 numbers = [str(x) for x in range(1,7)]
@@ -10,10 +11,14 @@ numbered_cards = []
 for color in colors:
     for number in numbers:
         for i in range(copies_of_number):
-            card = Card(color, number)
+            card = push.Card(color, number)
             numbered_cards.append(card)
 
 assert len(numbered_cards) == 90
 
-deck = Deck(numbered_cards)
-print(deck)
+active_card = push.Card("Green", '1')
+pile_1 = push.Pile([push.Card("Red", "1"), push.Card("Blue", "2")])
+pile_1.show()
+
+pile_1.check_availability(active_card)
+pile_1.show_availability(active_card)
---
+++ push.py
@@ -0,0 +1,81 @@
+import random
+
+
+class Card:
+    def __init__(self, color, number):
+        self.color = color
+        self.number = number
+        self.active = False
+
+    def __str__(self):
+        return "{color} {number}".format(color=self.color, number=self.number)
+
+    def play_card(self):
+        pass
+
+
+class Stack:
+    def __init__(self, card_stack):
+        self.card_stack = card_stack
+
+    def show(self):
+        for card in self.card_stack:
+            print(card)
+
+    def count_cards(self):
+        return "There are {num_cards} in this stack.".format(num_cards=len(self.card_stack))
+
+    def check_card_types(self):
+        card_tracker = {}
+        for card in self.card_stack:
+            card_value = "{color} {number}".format(color=card.color, number=card.number)
+            if card_value in card_tracker.keys():
+                card_tracker[card_value] += 1
+            else:
+                card_tracker[card_value] = 1
+        return card_tracker
+
+    def add_card_to_stack(self, card):
+        self.card_stack.append(card)
+
+
+class Deck(Stack):
+    def shuffle(self):
+        random.shuffle(self.card_stack)
+
+    def draw(self):
+        card = self.card_stack.pop()
+        card.in_play = True
+        return card
+
+
+class Pile(Stack):
+    def __init__(self, card_stack):
+        self.available = True
+        self.card_stack = card_stack
+
+    def check_availability(self, active_card):
+        self.available = True
+        for played_card in self.card_stack:
+            if active_card.color == played_card.color:
+                self.available = False
+            elif active_card.number == played_card.number:
+                self.available = False
+
+
+    def show_availability(self, active_card):
+        if self.available == True:
+            print(f"{active_card} can be played in this pile.".format(active_card))
+        else:
+            print(f"{active_card} cannot be played in this pile.".format(active_card))
+
+    def clear_stack(self):
+        self.card_stack = []
+
+
+class Dice:
+    pass
+
+
+class Player:
+    pass
