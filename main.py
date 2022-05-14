7d42bbb407ffa205593466515632ee7ee8c0c9c1
ryrodrig112 <ryrodrig112@gmail.com>
2022-05-13 20:23:44 -0700

    Separate game component classes from player classes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import push
+import components
 
 print("Hello World")
 print('')
@@ -6,16 +6,32 @@ print('')
 colors = ['Red', 'Blue', 'Yellow','Green', 'Purple']
 numbers = [str(x) for x in range(1,7)]
 copies_of_number = 3
+num_bombs = 18
+num_reverse = 12
 
+
+#instantiate cards
 numbered_cards = []
+bomb_list = []
+reverse_list = []
 for color in colors:
     for number in numbers:
         for i in range(copies_of_number):
-            card = push.Card(color, number)
+            card = components.Card(color, number)
             numbered_cards.append(card)
+for bomb in range(num_bombs):
+    card = components.Card("Bomb", "Bomb")
+    bomb_list.append(card)
+for reverse in range(num_reverse):
+    card = components.Card ("Reverse", "Reverse")
+    reverse_list.append(card)
 
-assert len(numbered_cards) == 90
+#instantiate deck
+deck = components.Deck(numbered_cards + bomb_list + reverse_list)
+deck.shuffle()
 
-dice = push.Dice(colors + ["Safe"])
-for i in range(50):
-    dice.roll()
+#instantiate 3 piles
+pile_1 = components.Pile([])
+pile_2= components.Pile([])
+pile_3 = components.Pile([])
+
