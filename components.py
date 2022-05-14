3a49c0809e4d2187f3b74d4f28e5587d6511c701
ryrodrig112 <ryrodrig112@gmail.com>
2022-05-13 20:23:56 -0700

    Separate game component classes from player classes

--- components.py
+++ components.py
@@ -80,15 +80,3 @@ class Dice:
         side = random.choice(self.sides)
         print(side)
         return side
-
-
-class Players():
-    def __init__(self, players, order):
-        self.players = players
-        self.order = order
-
-
-class Player:
-    def __init__(self, strategy, points):
-        self.strategy = strategy
-        self.points = points
