45f4e66176bcdb7b4b09b2e3a23cdb57259b6cb1
ryrodrig112 <ryrodrig112@gmail.com>
2022-05-23 13:34:24 -0400

    Restart players.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import components
+import players
+import strategies
 
 print("Hello World")
 print('')
 
-colors = ['Red', 'Blue', 'Yellow','Green', 'Purple']
-numbers = [str(x) for x in range(1,7)]
+# params
+colors = ['Red', 'Blue', 'Yellow', 'Green', 'Purple']
+numbers = [str(x) for x in range(1, 7)]
 copies_of_number = 3
 num_bombs = 18
 num_reverse = 12
+num_players = 4
 
-
-#instantiate cards
+# instantiate cards
+#
 numbered_cards = []
 bomb_list = []
 reverse_list = []
@@ -23,15 +27,35 @@ for bomb in range(num_bombs):
     card = components.Card("Bomb", "Bomb")
     bomb_list.append(card)
 for reverse in range(num_reverse):
-    card = components.Card ("Reverse", "Reverse")
+    card = components.Card("Reverse", "Reverse")
     reverse_list.append(card)
 
-#instantiate deck
+# instantiate deck
 deck = components.Deck(numbered_cards + bomb_list + reverse_list)
 deck.shuffle()
 
-#instantiate 3 piles
+# instantiate 3 piles
 pile_1 = components.Pile([])
-pile_2= components.Pile([])
+pile_2 = components.Pile([])
 pile_3 = components.Pile([])
+
+# instantiate players
+player_list = []
+for player in range(num_players):
+    name = "Player {}".format(player)
+    player = players.Player(name, strategies.random, None, None)
+    player_list.append(player)
+group = players.Players(player_list, 1)
+
+#play the game until there are no more cards in deck
+# while len(deck.card_stack) > 0:
+#     pass
+#     #play the game
+
+print(group)
+#
+
+
+
+
 
---
+++ strategies.py
@@ -0,0 +1,6 @@
+import random
+
+
+def random_strategy(options):
+    """Strategy that randomly chooses how to allocate cards among piles"""
+    return random.choice(options)
